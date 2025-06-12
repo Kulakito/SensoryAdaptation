68e55c1d083a743694ad4cdeda7d3e3780b55f1a
VolderMiles <161145456+VolderMiles@users.noreply.github.com>
2025-06-12 18:46:25 +0300

    Radio

---
+++ SensoryAdaptation/Assets/Scripts/Krutilki/Krutilka.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Krutilka : MonoBehaviour
+{
+    [SerializeField] float _nearity;
+    public float nearity
+    {
+        get { return _nearity; }
+        private set { _nearity = value; }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
